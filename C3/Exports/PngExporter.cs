9aec33bdbdcdcdd5d593f0ec84091d2d9644aca7
Santa <SantasCode@proton.me>
2022-12-05 22:58:25 -0900

    Support for dds to png conversion

---
+++ C3/Exports/PngExporter.cs
@@ -0,0 +1,23 @@
+﻿using BCnEncoder.Decoder;
+using BCnEncoder.ImageSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace C3.Exports
+{
+    public static class PngExporter
+    {
+        private static BcDecoder _decoder = new();
+        public static void Export(Stream input, Stream output)
+        {
+            using (Image<Rgba32> Image = _decoder.DecodeToImageRgba32(input))
+            {
+                var encoder = Image.GetConfiguration().ImageFormatsManager.FindEncoder(PngFormat.Instance);
+
+                Image.Save(output, encoder);
+            }
+        }
+    }
+}
